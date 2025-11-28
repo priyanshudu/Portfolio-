fd019d9e13c28541eb0cf5c1fe2b3807906d6b16
priyanshudu <158175764+priyanshudu@users.noreply.github.com>
2025-11-29 00:21:56 +0530

    Remove contact form demo from script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// Update year in footer
 document.getElementById('year').textContent = new Date().getFullYear();
-
-// Contact form demo
-document.getElementById('contactForm').addEventListener('submit', function(e){
-  e.preventDefault();
-  let name = document.getElementById('nameField').value;
-  let email = document.getElementById('emailField').value;
-  let msg = document.getElementById('msgField').value;
-  alert('Demo message sent!\nName: '+name+'\nEmail: '+email+'\nMessage: '+msg);
-  this.reset();
-});
